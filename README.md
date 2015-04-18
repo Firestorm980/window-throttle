8a7cebe9b724d8375ce4a2db090643a206ef9a81
Jon Christensen <jon@jmchristensendesign.com>
2015-04-17 22:50:12 -0500

    Version 1
    Demo HTML, updated plugin, updated readme

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
-# window-throttle
+jQuery Window Throttle
+======================
+##The overdone window event plugin
+
+jQuery Window Throttle (jQWT) normalizes and throttles the window scroll and resize events between browsers while also offering some great event data so that you stop calculating values and just get things done. jQWT uses either a custom interval timeout or Request Animation Frame to poll the window for any changes in size or scroll position. jQWT returns the following data:
+
+####Scroll
+- Scroll percentage down the page (event.percent)
+- Scroll delta from the previous polling position (event.delta)
+
+####Resize
+- Orientation, landscape or portrait (event.orientation)
+- A boolean of if the width or height changed (event.changed.width, event.changed.height)
+- Delta in width and height from previous polling position (event.delta.width, event.delta.height)
+- The new dimensions of the window (event.dimensions.width, event.dimensions.height)
+
+#Getting Started
+####Installation
+Load the jQWT script after you load jQuery on your pages and initiate it.
+
+```HTML
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="/path-to-source/jquery.windowThrottle.js"></script>
+<script>
+	jQuery(document).ready(function($) {
+		$.windowThrottle();
+	});
+</script>
+```
+
+####Use
+It's important that you bind to the "throttle.resize" and "throttle.scroll" events before starting up the plugin. This way, the events will fire immediately when the page is ready, allowing your scripts to work their magic as soon as possible. You'll want to bind the window to the custom events that come from jQWT like so: 
+```HTML
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="/path-to-source/jquery.windowThrottle.js"></script>
+<script>
+	jQuery(document).ready(function($) {
+		jQuery(window).on('throttle.resize', function(event){ 
+			// Do your stuff here
+			// Access the data using the event object (ex. "event.orientation")
+		});
+		jQuery(window).on('throttle.scroll', function(event){ 
+			// Do your stuff here
+			// Access the data using the event object (ex. "event.delta")
+		});
+
+		$.windowThrottle();
+	});
+</script>
+```
+
+#####Event Data
+Here are two examples of the event data returned when one of the custom events is triggered.
+
+######Scrolling
+```Javascript
+event: {
+	delta: 0,
+	percent: 0	
+}
+```
+######Resize
+```Javascript
+event: {
+	changed: { width: false, height: false },
+	dimensions: { width: 1280, height: 720 },
+	delta: { width: 0, height: 0 },
+	orientation: 'landscape'
+}
+```
+
+####Options
+These are the options currently available for jQWT:
+
+| Option       | Type   | Default        | Description                                                             |
+|--------------|--------|----------------|-------------------------------------------------------------------------|
+| `detectResize`  | `boolean` | `true`   | Bind the resize event               |
+| `detectScroll`  | `boolean` | `true`   | Bind the scroll event |
+| `pollingTime`   | `number`  | `150`    | Set the amount of time between polling in milliseconds. Disabled if `useRAF` is `true`. |
+| `useRAF`        | `boolean` | `false`  | Use window.requestAnimationFrame. Overrides `pollingTime`. Falls back to setTimeout in older browsers. |
--- src/jquery.windowThrottle.js
+++ src/jquery.windowThrottle.js
@@ -2,7 +2,7 @@
  * jQuery Window Throttle
  * @author: Jon Christensen (Firestorm980)
  * @github: https://github.com/Firestorm980/WindowThrottle
- * @version: 0.1
+ * @version: 1.0
  *
  * Licensed under the MIT License.
  */
@@ -15,8 +15,10 @@
 	// Create the defaults once
 	var defaults = {
 		// Options
+		detectResize: true, // Set to detect resize
+		detectScroll: true, // Set to detect scrolling
 		pollingTime: 150, // Uses setInterval
-		useRAF: true // Uses requestAnimationFrame
+		useRAF: false // Uses requestAnimationFrame. Falls back to a timeout.
 	};
 
 	$.windowThrottle = function(options){
@@ -27,7 +29,8 @@
 			hasScrolled: false,
 			width: 0,
 			height: 0,
-			scrollPosition: 0
+			scrollPosition: 0,
+			orientation: null
 		};
 
 		var methods = {
@@ -39,21 +42,52 @@
 			init: function(){
 				var $window = jQuery(window);
 
-				$window.on('scroll', function(){ windowData.hasScrolled = true; });
-				$window.on('resize', function(){ windowData.hasResized = true; });
+				// Set rAF support
+				// Normalizes the event through all browsers
+				methods.setRAF();
 
-				windowData.width = $window.width();
-				windowData.height = $window.height();
-
-				methods.resize.init();
-				methods.scroll.init();
+				// Resize
+				if ( settings.detectResize === true ){
+					// Bind Events
+					// Use resize and orientation change for mobile browsers
+					$window.on('resize orientationchange', function(){ windowData.hasResized = true; });
+					// Set starting width and height
+					windowData.width = $window.width();
+					windowData.height = $window.height();
+					// Set starting orientation
+					methods.resize.setOrientation( windowData.width, windowData.height );
+					// Start up the resize function
+					methods.resize.event(); // Run on page load
+				}
+				// Scrolling
+				if ( settings.detectScroll === true ){
+					// Bind event
+					// Use scroll and touchmove for older mobile browsers
+					$window.on('scroll touchmove', function(){ windowData.hasScrolled = true; });	
+					// Set starting scroll position
+					windowData.scrollPosition = $window.scrollTop();	
+					// Start it up
+					methods.scroll.event();
+				}
+				// Start polling
+				methods.poll();
+			},
+			setRAF: function(){
+				window.requestAnimFrame = (function(callback) {
+				return 	window.requestAnimationFrame || 
+						window.webkitRequestAnimationFrame || 
+						window.mozRequestAnimationFrame || 
+						window.oRequestAnimationFrame || 
+						window.msRequestAnimationFrame ||
+						// Fallback to a timeout in older browsers
+						function(callback) {
+							window.setTimeout(callback, 1000 / 60);
+						};
+				})();
 			},
 			resize: {
-				init: function(){
-					methods.resize.event(); // Run on page load
-					methods.poll();
-				},
 				event: function(){
+					// Calculations
 					var 
 						$window = jQuery(window),
 						newWidth = $window.width(),
@@ -61,62 +95,92 @@
 						oldWidth = windowData.width,
 						oldHeight = windowData.height,
 						widthChanged = ( newWidth !== oldWidth ),
-						heightChanged = ( newHeight !== oldHeight );
-
+						heightChanged = ( newHeight !== oldHeight ),
+						widthDelta = newWidth - oldWidth,
+						heightDelta = newHeight - oldHeight;
+					// Set orientation
+					methods.resize.setOrientation( newWidth, newHeight );
+					// Our event data
 					var eventObject = {
 						type: 'throttle.resize',
-						widthChanged: widthChanged,
-						heightChanged: heightChanged,
-						width: newWidth,
-						height: newHeight
+						changed: { width: widthChanged, height: heightChanged },
+						dimensions: { width: newWidth, height: newHeight },
+						delta: { width: widthDelta, height: heightDelta },
+						orientation: methods.resize.getOrientation()
 					};
-
+					// Kick off the event
 					$window.trigger( eventObject );
-					windowData.width = $window.width();
-					windowData.height = $window.height();
+					// Update the window data
+					windowData.width = newWidth;
+					windowData.height = newHeight;
+				},
+				setOrientation: function( width, height ){
+					// Use match media if available. Much more accurate.
+					if ( window.matchMedia !== undefined ){
+						if ( window.matchMedia("(orientation: landscape)").matches ){ windowData.orientation = 'landscape'; }
+						if ( window.matchMedia("(orientation: portrait)").matches ){ windowData.orientation = 'portrait'; }
+					}
+					// Fallback to just using the dimensions
+					else {
+						if ( width > height || width === height ){ windowData.orientation = 'landscape'; }
+						if ( width < height ){ windowData.orientation = 'portrait'; }
+					}
+				},
+				getOrientation: function(){
+					// Use match media if available. Much more accurate.
+					if ( window.matchMedia !== undefined ){
+						if ( window.matchMedia("(orientation: landscape)").matches ){ return 'landscape'; }
+						if ( window.matchMedia("(orientation: portrait)").matches ){ return 'portrait'; }
+					}
+					// Fallback to just using the dimensions
+					else {
+						if ( windowData.width > windowData.height || windowData.width === windowData.height ){ return 'landscape'; }
+						if ( windowData.width < windowData.height ){ return 'portrait'; }
+					}
 				}
 			},
 			scroll: {
-				init: function(){
-					methods.scroll.event(); // Run on page load
-					methods.poll();
-				},
 				event: function(){
+					// Calculations
 					var 
 						$window = jQuery(window),
 						scrollTop = $window.scrollTop(),
 						documentHeight = jQuery(document).height(),
-						scrollPercent = scrollTop / documentHeight,
-						scrollDelta = windowData.scrollPosition - scrollTop;
-
+						scrollPercent = Math.round( ( scrollTop / ( documentHeight - windowData.height ) ) * 100 ),
+						scrollDelta = scrollTop - windowData.scrollPosition;
+					// Our event data
 					var eventObject = {
 						type: 'throttle.scroll',
 						delta: scrollDelta,
 						percent: scrollPercent
 					};
-
+					// Kick off the event
 					$window.trigger( eventObject );
+					// Update the window data
+					windowData.scrollPosition = scrollTop;
 				}
 			},
 			poll: function(){
-				var polling = false;
+				// Check if we're using rAF
 				if ( settings.useRAF ){
-					methods.checkForChanges();
-					requestAnimationFrame( methods.poll );
+					methods.checkForChanges(); // Check for changes
+					requestAnimFrame( methods.poll ); // Run another poll when ready
 				}
-				else if ( !polling ){
-					polling = true;
+				// Use setInterval instead
+				else {
 					setInterval( methods.checkForChanges, settings.pollingTime);
 				}
 			},
 			checkForChanges: function(){
-				if ( windowData.hasScrolled ){
-					methods.scroll.event();
-					windowData.hasScrolled = false;
+				// Check if we've scrolled and if we're interested
+				if ( windowData.hasScrolled && settings.detectScroll === true ){
+					methods.scroll.event(); // Do our scroll event
+					windowData.hasScrolled = false; // Reset the boolean
 				}
-				if ( windowData.hasResized ){
-					methods.resize.event();
-					windowData.hasResized = false;
+				// Check if we've resized and if we're interested
+				if ( windowData.hasResized && settings.detectResize === true ){
+					methods.resize.event(); // Do our resize event
+					windowData.hasResized = false; // Reset the boolean
 				}
 			}
 		};
@@ -126,19 +190,7 @@
 
 		// Public Methods
 		return {
-			/**
-			 * load
-			 * ----
-			 * Programically load in a new page. Treats it like a link, adding a new history entry.
-			 * 
-			 * @param  {[string]} url [The URL of the page to load.]
-			 */
-			load: function(url){
-				var samePage = checkSamePage( currentLocation, url);
-				if ( typeof url === 'string' && !samePage){
-					methods.loadResource(url, true);
-				}
-			}
+
 		};
 	};
 })( jQuery );
